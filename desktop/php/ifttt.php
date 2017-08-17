986d39704086072aae936a90fda4681c6543a56f
Loïc <loic@jeedom.com>
2017-08-17 15:43:34 +0200

    review ui

--- desktop/php/ifttt.php
+++ desktop/php/ifttt.php
@@ -2,8 +2,9 @@
 if (!isConnect('admin')) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
-sendVarToJS('eqType', 'ifttt');
-$eqLogics = eqLogic::byType('ifttt');
+$plugin = plugin::byId('ifttt');
+sendVarToJS('eqType', $plugin->getId());
+$eqLogics = eqLogic::byType($plugin->getId());
 ?>
 
 <div class="row row-overflow">
@@ -27,10 +28,10 @@ foreach ($eqLogics as $eqLogic) {
   <div class="eqLogicThumbnailContainer">
     <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
       <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
-    <br>
-    <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">Ajouter</span>
-  </div>
-  <?php
+      <br>
+      <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">Ajouter</span>
+    </div>
+    <?php
 
 foreach ($eqLogics as $eqLogic) {
 	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
@@ -41,82 +42,86 @@ foreach ($eqLogics as $eqLogic) {
 	echo '</div>';
 }
 ?>
-</div>
+ </div>
 </div>
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-  <form class="form-horizontal">
-    <fieldset>
-      <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-      <div class="form-group">
-        <label class="col-sm-3 control-label">{{Nom de l'équipement IFTTT}}</label>
-        <div class="col-sm-3">
-          <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-          <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement IFTTT}}"/>
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-3 control-label" >{{Objet parent}}</label>
-        <div class="col-sm-3">
-          <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-            <option value="">{{Aucun}}</option>
-            <?php
+  <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+  <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+  <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+  </ul>
+  <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+    <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+      <br/>
+      <form class="form-horizontal">
+        <fieldset>
+          <div class="form-group">
+            <label class="col-sm-3 control-label">{{Nom de l'équipement IFTTT}}</label>
+            <div class="col-sm-3">
+              <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+              <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement IFTTT}}"/>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+            <div class="col-sm-3">
+              <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                <option value="">{{Aucun}}</option>
+                <?php
 foreach (object::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
-         </select>
-       </div>
-     </div>
-     <div class="form-group">
-      <label class="col-sm-3 control-label"></label>
-      <div class="col-sm-9">
-        <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-        <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-      </div>
-    </div>
-    <div class="form-group">
-     <label class="col-sm-3 control-label">{{Catégorie}}</label>
-     <div class="col-sm-8">
-      <?php
+             </select>
+           </div>
+         </div>
+         <div class="form-group">
+          <label class="col-sm-3 control-label"></label>
+          <div class="col-sm-9">
+            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+          </div>
+        </div>
+        <div class="form-group">
+         <label class="col-sm-3 control-label">{{Catégorie}}</label>
+         <div class="col-sm-8">
+          <?php
 foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
 	echo '<label class="checkbox-inline">';
 	echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
 	echo '</label>';
 }
 ?>
-   </div>
- </div>
- <div class="form-group">
-  <label class="col-sm-3 control-label">{{Clef}}</label>
-  <div class="col-sm-3">
-    <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="key" />
-  </div>
-</div>
-</fieldset>
-</form>
-
-<legend><i class="fa fa-list-alt"></i> {{IFTTT}}</legend>
-<div class="alert alert-info">Exemple d’URL à appeler : <?php echo network::getNetworkAccess('external') ?>/core/api/jeeApi.php?api=<?php echo config::byKey('api'); ?>&type=ifttt&id=#ID_CMD#&value=#VALEUR#</div>
-<a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une commande}}</a><br/><br/>
-<table id="table_cmd" class="table table-bordered table-condensed">
-  <thead>
-    <tr>
-      <th>{{#}}</th><th>{{Nom}}</th><th>{{Sous-type}}</th><th style="width:200px;">{{Evènement}}</th><th>{{Valeur}}</th><th>{{Options}}</th><th style="width:200px;">{{Action}}</th>
-    </tr>
-  </thead>
-  <tbody>
-  </tbody>
-</table>
-
-<form class="form-horizontal">
-  <fieldset>
-    <div class="form-actions">
-      <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-      <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+       </div>
+     </div>
+     <div class="form-group">
+      <label class="col-sm-3 control-label">{{Clef}}</label>
+      <div class="col-sm-3">
+        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="key" />
+      </div>
     </div>
   </fieldset>
 </form>
+</div>
+<div role="tabpanel" class="tab-pane" id="commandtab">
+  <br/>
+  <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une commande}}</a>
+  <br/><br/>
+  <div class="alert alert-info">Exemple d’URL à appeler : <?php echo network::getNetworkAccess('external') ?>/core/api/jeeApi.php?api=<?php echo jeedom::getApiKey('ifttt'); ?>&type=ifttt&id=#ID_CMD#&value=#VALEUR#</div>
+  <table id="table_cmd" class="table table-bordered table-condensed">
+    <thead>
+      <tr>
+        <th>{{#}}</th><th>{{Nom}}</th><th>{{Sous-type}}</th><th>{{Valeur}}</th><th>{{Options}}</th><th style="width:200px;">{{Action}}</th>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+
 
 </div>
 </div>
